15d73aef3c4ffd9e91155b7c37404fad9d1ec4af
Zayvius Digital <zayviusdigital@gmail.com>
2023-01-19 20:09:54 +0700

    git fetch origin --prune

--- app/src/main/java/com/kepoyah/gaide/MainActivity.java
+++ app/src/main/java/com/kepoyah/gaide/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
